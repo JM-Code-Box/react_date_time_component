13409beae60eac20a4aaef2e04c7ac58acaa6980
Justin Meyer <32879771+justin-codes@users.noreply.github.com>
2018-01-26 22:43:03 -0600

    adding JS file for component

---
+++ DateTime.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+
+class clock extends Component {
+  
+    state = {
+        time: new Date().toLocaleTimeString(),
+        date: new Date().toLocaleDateString()
+    }
+
+    getTime = () => {
+        const takeTwelve = (n) => n > 12 ? n - 12 : n;
+        const addZero = (n) => n < 10 ? '0' + n : n;
+        const amPm = (n) => n > 12 ? 'pm' : 'am';
+        
+        let getDate = new Date();
+        let h = addZero(takeTwelve(getDate.getHours()));
+        let m = addZero(getDate.getMinutes());
+        let s = addZero(getDate.getSeconds());
+        let tale = amPm(getDate.getHours());
+        let timeString = `${h}:${m}:${s} ${tale}`;
+        this.setState(
+            { time: timeString }
+        );
+    
+    };
+
+    componentWillMount() {
+        setInterval(
+            () => this.getTime(), 1000);
+    }
+
+    render() {
+        return (
+        <div>
+            <p>{this.state.date} | {this.state.time}</p>
+        </div>
+        )
+    }
+}
+
+
+
+
+
+export default clock;
